19daa4bb531c1fddcb41f3c9f73253dd574e86d9
herreraEd <mapisherrera@misena.edu.co>
2020-09-20 17:52:35 -0500

    add name

--- app.js
+++ app.js
@@ -18,4 +18,16 @@ app.get('/skills', (req, res) => {
   res.send('skills')
 })
 
+app.get('/skills', (req, res) => {
+  res.send('skills')
+})
+
+app.get('/new', (req, res) => {
+  res.send('new page')
+})
+
+app.get('/name', (req, res) => {
+  res.send('<h1>John</h1>')
+})
+
 app.listen(process.env.PORT || 3000)
